9f3c65d6d5bb3f94efa494495ead1b77574a5f4c
刘磊 <lei.liu@goland.cn>
2024-04-07 13:05:03 +0800

    1

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,13 +23,16 @@ function Home() {
           <p>The 15th International Conference in Evolutionary Linguistics</p>
           <p>第15届演化语言学国际研讨会</p>
         </div>
-        <div className={styles.date}>
-          <p>Dates: July 12-14, 2024</p>
-          <p>时间：2024年7月12日-14日</p>
-        </div>
-        <div className={styles.position}>
-          <p>Venue：Hunan University</p>
-          <p>地点：湖南大学</p>
+        <div className={styles.content}>
+        <div className={styles.logo}></div>
+          <div className={styles.date}>
+            <p>Dates: July 12-14, 2024</p>
+            <p>时间：2024年7月12日-14日</p>
+          </div>
+          <div className={styles.position}>
+            <p>Venue：Hunan University</p>
+            <p>地点：湖南大学</p>
+          </div>
         </div>
       </div>
     </div>
